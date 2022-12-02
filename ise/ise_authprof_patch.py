f641cd417bdf75fda551e8147a0eb9f534ec0196
p3rh0ps <p3rh0ps@gmail.com>
2022-12-02 12:13:39 +0100

    #Beta 0001

--- ise/ise_authprof_patch.py
+++ ise/ise_authprof_patch.py
@@ -17,9 +17,10 @@ from urllib3 import disable_warnings
 disable_warnings(InsecureRequestWarning)
 
 API_PROTO  = "https"
-ISE_HOST = "10.0.0.1"
-API_PORT = "443"
+ISE_HOST = "64.103.46.211"
+API_PORT = "9060"
 RESOURCE = "ers/config/authorizationprofile"
+METHOD = "PUT"
 
 url = f"{API_PROTO}://{ISE_HOST}:{API_PORT}/{RESOURCE}"
 auth = "ers","MySuperPassword"
@@ -38,42 +39,69 @@ def retrieve_auth_profiles(url) -> list:
         resp_get = requests.get(\
         url, auth=auth, headers=headers, data=payload_get, verify=False)
         auth_profiles += resp_get.json()["SearchResult"]["resources"]
-        print(json.dumps(auth_profiles, indent=4))
         try :
             resp_get.raise_for_status()
             url = resp_get.json()["SearchResult"]["nextPage"]["href"]
         except Exception as e :
-            print(e)
             url = None
     return auth_profiles
 
+def retrieve_auth_profile_config(prof_id: str) -> dict:
+    """ Retrieve Authorization Profile Configuration
+        By ID
+    """
+    payload_get = {}
+    RESOURCE_GET = f"ers/config/authorizationprofile/{prof_id}"
+    url = f"{API_PROTO}://{ISE_HOST}:{API_PORT}/{RESOURCE_GET}"
+    
+    resp_get_conf_id = requests.get(\
+    url, auth=auth, headers=headers, data=payload_get, verify=False)
+    print(json.dumps(resp_get_conf_id.json(), indent=4))
+    return resp_get_conf_id.json()
 
-def patch_auth_profile(prof_id: str, payload: dict) -> None:
+
+def update_auth_profile(method: str, prof_id: str, payload: dict) -> None:
     """ Patch Authorization Profiles by ID in ISE
         with specific payload_patch
-    """ 
-    RESOURCE_PATCH = f'ers/config/authorizationprofile/{prof_id}'
-    url = f"{API_PROTO}://{ISE_HOST}:{API_PORT}/{RESOURCE_PATCH}"
+    """
+    RESOURCE = f'ers/config/authorizationprofile/{prof_id}'
 
-    resp_patch = requests.patch(\
-    url, auth=auth, headers=headers, data=payload, verify=False)
-    resp_patch.raise_for_status()
+    url = f"{API_PROTO}://{ISE_HOST}:{API_PORT}/{RESOURCE}"
+    
+    resp = requests.request(\
+    method, url, auth=auth, headers=headers, data=payload, verify=False)
+    print(resp.request.url)
+    print(resp.request.headers)
+    resp.raise_for_status()
+
 
 def main():
-
+    """ Main Function
+    """
     authprof_lst = retrieve_auth_profiles(url)
 
-    payload_patch = {
-           "AuthorizationProfile": {
-             "reauth" : {
-               "timer" : 65000,
+    change_to_pass = {
+            "reauth" : {
+               "timer" : 10000,
                "connectivity" : "RADIUS_REQUEST"
                }
              }
-           }
+
+    payload_patch = { "AuthorizationProfile":  {
+                        }
+                    }
+
+    payload_patch["AuthorizationProfile"].update(change_to_pass)
 
     for idx in range(0, len(authprof_lst)):
-        patch_auth_profile(authprof_lst[idx]["id"], payload_patch)
+        if METHOD == "PATCH":
+            update_auth_profile(METHOD,authprof_lst[idx]["id"], payload_patch)
+        if METHOD == "PUT":
+            payload_put = retrieve_auth_profile_config(authprof_lst[idx]["id"])
+            payload_put["AuthorizationProfile"].update(change_to_pass)
+            print(json.dumps(payload_put, indent=4))
+            #print(payload_put)
+            update_auth_profile(METHOD,authprof_lst[idx]["id"], payload_put)
 
 if __name__ == "__main__":
     main()
