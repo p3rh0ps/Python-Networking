36b0b1ba60ce02903e689303b40d0b6992a16e83
p3rh0ps <p3rh0ps@gmail.com>
2022-12-02 16:31:42 +0100

    Beta#0002 PUT method OK waiting for ISE upgrade for PATCH

--- ise/ise_authprof_patch.py
+++ ise/ise_authprof_patch.py
@@ -12,13 +12,14 @@ __status__ = "prototype"
 
 import json
 import requests
+import re
 from urllib3.exceptions import InsecureRequestWarning
 from urllib3 import disable_warnings
 disable_warnings(InsecureRequestWarning)
 
 API_PROTO  = "https"
-ISE_HOST = "64.103.46.211"
-API_PORT = "9060"
+ISE_HOST = "64.103.46.137"
+API_PORT = "443"
 RESOURCE = "ers/config/authorizationprofile"
 METHOD = "PUT"
 
@@ -28,7 +29,8 @@ headers = {
     'Content-type': 'application/json',
     'Accept': 'application/json',
 }
-    
+
+
 def retrieve_auth_profiles(url) -> list:
     """ Retrieve Authorization Profiles from ISE
     """
@@ -37,7 +39,7 @@ def retrieve_auth_profiles(url) -> list:
 
     while (url) :
         resp_get = requests.get(\
-        url, auth=auth, headers=headers, data=payload_get, verify=False)
+        url, auth=auth, headers=headers, data=json.dumps(payload_get), verify=False)
         auth_profiles += resp_get.json()["SearchResult"]["resources"]
         try :
             resp_get.raise_for_status()
@@ -45,6 +47,7 @@ def retrieve_auth_profiles(url) -> list:
         except Exception as e :
             url = None
     return auth_profiles
+
 
 def retrieve_auth_profile_config(prof_id: str) -> dict:
     """ Retrieve Authorization Profile Configuration
@@ -55,7 +58,7 @@ def retrieve_auth_profile_config(prof_id: str) -> dict:
     url = f"{API_PROTO}://{ISE_HOST}:{API_PORT}/{RESOURCE_GET}"
     
     resp_get_conf_id = requests.get(\
-    url, auth=auth, headers=headers, data=payload_get, verify=False)
+    url, auth=auth, headers=headers, data=json.dumps(payload_get), verify=False)
     print(json.dumps(resp_get_conf_id.json(), indent=4))
     return resp_get_conf_id.json()
 
@@ -69,9 +72,8 @@ def update_auth_profile(method: str, prof_id: str, payload: dict) -> None:
     url = f"{API_PROTO}://{ISE_HOST}:{API_PORT}/{RESOURCE}"
     
     resp = requests.request(\
-    method, url, auth=auth, headers=headers, data=payload, verify=False)
-    print(resp.request.url)
-    print(resp.request.headers)
+    method, url, auth=auth, headers=headers, data=json.dumps(payload), verify=False)
+    print("****",resp.status_code,"****")
     resp.raise_for_status()
 
 
@@ -96,11 +98,9 @@ def main():
     for idx in range(0, len(authprof_lst)):
         if METHOD == "PATCH":
             update_auth_profile(METHOD,authprof_lst[idx]["id"], payload_patch)
-        if METHOD == "PUT":
+        if METHOD == "PUT" and not re.match("(Deny|Permit)Access", authprof_lst[idx]["name"]):
             payload_put = retrieve_auth_profile_config(authprof_lst[idx]["id"])
             payload_put["AuthorizationProfile"].update(change_to_pass)
-            print(json.dumps(payload_put, indent=4))
-            #print(payload_put)
             update_auth_profile(METHOD,authprof_lst[idx]["id"], payload_put)
 
 if __name__ == "__main__":
