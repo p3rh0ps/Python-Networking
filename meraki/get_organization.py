de01596f17551e226cf5b2ab38e6327ea78cb8ac
p3rh0ps <p3rh0ps@gmail.com>
2023-02-02 14:44:34 +0100

    Change to operate on many org

--- meraki/get_organization.py
+++ meraki/get_organization.py
@@ -1,6 +1,7 @@
 import requests
 
-token = '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'
+#token = '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'
+token = 'fd6dd87d96915f21bc0e0b3d96a866ff0e53e381'
 
 headers = {
     'X-Cisco-Meraki-API-Key': token,
@@ -14,7 +15,8 @@ base_url = "https://api.meraki.com/api/v0"
 def get_organizations():
     orgs_url = '/organizations'
     response = requests.get(base_url + orgs_url, headers=headers)
-    print(response.json()['id'])
+    for idx in range(0, len(response.json())):
+        print(response.json()[idx]['id'])
 
 if __name__ == "__main__":
     get_organizations()
