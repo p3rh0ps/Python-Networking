3389638dca11f66dc4865c76dcffd6778b50a815
p3rh0ps <33998785+p3rh0ps@users.noreply.github.com>
2025-02-03 22:30:36 +0100

    Create device_info.py

---
+++ pyats/error_counters/device_info.py
@@ -0,0 +1,26 @@
+pyats-1 = {
+             "address": "172.16.160.159",
+             "netconf_port": 830,
+             "restconf_port": 443,
+             "ssh_port": 22,
+             "username": "admin",
+             "password": "secret"
+          }
+
+pyats-2 = {
+             "address": "172.16.160.160",
+             "netconf_port": 830,
+             "restconf_port": 443,
+             "ssh_port": 22,
+             "username": "admin",
+             "password": "secret"
+          }
+
+pyats-3 = {
+             "address": "172.16.160.161",
+             "netconf_port": 830,
+             "restconf_port": 443,
+             "ssh_port": 22,
+             "username": "admin",
+             "password": "secret"
+          }
